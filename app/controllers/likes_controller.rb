853478886e5294a412cc6f3636af39acfb9cf4c2
Egor1007-del <hyinm2012@yandex.ru>
2026-03-12 22:17:45 +0100

    update controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -15,7 +15,7 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    @like = current_user.likes.find_by(params[:id])
+    @like = current_user.likes.find_by(id: params[:id])
     @like&.destroy
     redirect_to post_path(@post), notice: t('like.removed')
   end
