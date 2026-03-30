38bfcd5b0bb572479349aae0092d9b6da58461ae
Egor1007-del <hyinm2012@yandex.ru>
2026-03-30 13:23:53 +0200

    add constant POST_PREVIEW_LENGTH

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
+  POST_PREVIEW_LENGTH = 220
+
   belongs_to :category, inverse_of: :posts
   belongs_to :creator, class_name: 'User', inverse_of: :posts
   has_many :comments, class_name: 'PostComment', dependent: :destroy, inverse_of: :post
@@ -8,4 +10,8 @@ class Post < ApplicationRecord
 
   validates :title, presence: true, length: { minimum: 3, maximum: 100 }
   validates :body, presence: true, length: { minimum: 10, maximum: 500 }
+
+  def preview
+    body.truncate(POST_PREVIEW_LENGTH)
+  end
 end
